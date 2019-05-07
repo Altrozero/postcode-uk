961624e1e9e3b16314edbe453e89e3dfa2c8656a
Altrozero <altrozero@gmail.com>
2019-05-07 16:23:33 +0100

    Added pull postcode from a string

--- src/Postcode.php
+++ src/Postcode.php
@@ -23,145 +23,183 @@ namespace Altrozero\PostcodeUK;
  */
 class Postcode
 {
-  /**
-   * Inputted postcode
-   *
-   * @var string $postcode
-   */
-  private $_postcode;
+    /**
+     * Inputted postcode
+     *
+     * @var string $postcode
+     */
+    private $_postcode;
 
-  /**
-   * Postcode constructor.
-   *
-   * Will throw an error if not a validate Postcode
-   *
-   * @param $postcode
-   */
-  public function __construct($postcode)
-  {
-    if (empty($postcode) || !self::validate($postcode)) {
-      throw new \InvalidArgumentException;
+    /**
+     * Postcode constructor.
+     *
+     * Will throw an error if not a validate Postcode
+     *
+     * @param $postcode
+     */
+    public function __construct($postcode)
+    {
+        if (empty($postcode) || !self::validate($postcode)) {
+            throw new \InvalidArgumentException;
+        }
+
+        $this->_postcode = self::format($postcode);
     }
 
-    $this->_postcode = self::format($postcode);
-  }
-
-  /**
-   * Get the postcode
-   *
-   * @return string
-   */
-  public function get()
-  {
-    return (string)$this->_postcode;
-  }
-
-  /**
-   * Get the Outward
-   *
-   * @return string
-   */
-  public function getOutward()
-  {
-    return substr($this->_postcode, 0, -3);
-  }
-
-  /**
-   * Get the Inward
-   *
-   * @return string
-   */
-  public function getInward()
-  {
-    return substr($this->_postcode, -3);
-  }
-
-  /**
-   * Get the Area
-   *
-   * @return string
-   */
-  public function getArea()
-  {
-    if (is_numeric(substr($this->_postcode, 1, 1))) {
-      return substr($this->_postcode, 0, 1);
+    /**
+     * Get the postcode
+     *
+     * @return string
+     */
+    public function get()
+    {
+        return (string)$this->_postcode;
     }
 
-    return substr($this->_postcode, 0, 2);
-  }
-
-  /**
-   * Get the district
-   *
-   * @return string
-   */
-  public function getDistrict()
-  {
-    $start = 1;
-
-    if (!is_numeric(substr($this->_postcode, 1, 1))) {
-      $start = 2;
+    /**
+     * Get the Outward
+     *
+     * @return string
+     */
+    public function getOutward()
+    {
+        return substr($this->_postcode, 0, -3);
     }
 
-    return substr($this->_postcode, $start, strlen($this->_postcode) - $start - 3);
-  }
-
-  /**
-   * Get the sector
-   *
-   * @return string
-   */
-  public function getSector()
-  {
-    return substr($this->_postcode, -3, 1);
-  }
-
-  /**
-   * Get the sector
-   *
-   * @return string
-   */
-  public function getUnit()
-  {
-    return substr($this->_postcode, -2);
-  }
-
-  /**
-   * Validate a UK postcode
-   *
-   * @param string $postcode
-   *
-   * @return boolean
-   */
-  public static function validate($postcode)
-  {
-    // Clean the input
-    $postcode = self::format($postcode);
-
-    if (
-      preg_match("/^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] 0[Aa]{2})$/", $postcode)
-    ) {
-      return true;
+    /**
+     * Get the Inward
+     *
+     * @return string
+     */
+    public function getInward()
+    {
+        return substr($this->_postcode, -3);
     }
 
-    return false;
-  }
+    /**
+     * Get the Area
+     *
+     * @return string
+     */
+    public function getArea()
+    {
+        if (is_numeric(substr($this->_postcode, 1, 1))) {
+            return substr($this->_postcode, 0, 1);
+        }
 
-  /**
-   * Format a postcode
-   * - Strip spaces
-   * - Make it uppercase
-   * - Does not validate
-   *
-   * @param string $postcode
-   *
-   * @return string
-   */
-  public static function format($postcode)
-  {
-    $postcode = preg_replace('/\s+/', '', $postcode);
-    $postcode = strtoupper($postcode);
+        return substr($this->_postcode, 0, 2);
+    }
 
-    return $postcode;
-  }
+    /**
+     * Get the district
+     *
+     * @return string
+     */
+    public function getDistrict()
+    {
+        $start = 1;
+
+        if (!is_numeric(substr($this->_postcode, 1, 1))) {
+            $start = 2;
+        }
+
+        return substr($this->_postcode, $start, strlen($this->_postcode) - $start - 3);
+    }
+
+    /**
+     * Get the sector
+     *
+     * @return string
+     */
+    public function getSector()
+    {
+        return substr($this->_postcode, -3, 1);
+    }
+
+    /**
+     * Get the sector
+     *
+     * @return string
+     */
+    public function getUnit()
+    {
+        return substr($this->_postcode, -2);
+    }
+
+    /**
+     * Validate a UK postcode
+     *
+     * @param string $postcode
+     *
+     * @return boolean
+     */
+    public static function validate($postcode)
+    {
+        // Clean the input
+        $postcode = self::format($postcode);
+
+        if (
+            preg_match("/^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] 0[Aa]{2})$/", $postcode)
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Format a postcode
+     * - Strip spaces
+     * - Make it uppercase
+     * - Does not validate
+     *
+     * @param string $postcode
+     *
+     * @return string
+     */
+    public static function format($postcode)
+    {
+        $postcode = preg_replace('/\s+/', '', $postcode);
+        $postcode = strtoupper($postcode);
+
+        return $postcode;
+    }
+
+    /**
+     * Find a postcode in a string
+     *
+     * @param string $term
+     *
+     * @return Postcode | null
+     */
+    public static function findInString(string $term)
+    {
+        // Replace anything that isn't alphanumeric or a space with a space
+        $term = preg_replace('/[^A-Za-z0-9\s]/', ' ', $term);
+
+        $parts = explode(' ', $term);
+
+        $parts = array_filter($parts, function($val) {
+            $val = trim($val);
+            return !empty($val);
+        });
+        $parts = array_values($parts);
+
+        $previousPart = '';
+
+        foreach($parts as $part) {
+            if (self::validate($part)) {
+                return new Postcode($part);
+            }
+
+            $toTest = trim($previousPart) . trim($part);
+            if (self::validate($toTest)) {
+                return new Postcode($toTest);
+            }
+
+            $previousPart = $part;
+        }
+
+        return null;
+    }
 }
--- test/PostcodeTest.php
+++ test/PostcodeTest.php
@@ -16,115 +16,179 @@ use PHPUnit\Framework\TestCase;
 
 class PostcodeTest extends TestCase
 {
-  public function inputPostcodesValidator()
-  {
-    return [
-      ['NE29 0EG', true],
-      ['CT13 9FD', true],
-      ['B37 7UA', true],
-      ['W1H 2LQ', true],
-      ['W2 1JB', true],
-      ['NE290EG', true],
-      ['CT139FD', true],
-      ['B377UA', true],
-      ['W1H2LQ', true],
-      ['BANGARANG', false],
-      ['C1 11', false],
-      ['BHK37 7UA', false],
-      ['W1H 22LQ', false],
-      ['W122LQFD', false],
-      ['W1 H2H', false],
-    ];
-  }
+    public function inputPostcodesValidator()
+    {
+        return [
+            ['NE29 0EG', true],
+            ['CT13 9FD', true],
+            ['B37 7UA', true],
+            ['W1H 2LQ', true],
+            ['W2 1JB', true],
+            ['NE290EG', true],
+            ['CT139FD', true],
+            ['B377UA', true],
+            ['W1H2LQ', true],
+            ['BANGARANG', false],
+            ['C1 11', false],
+            ['BHK37 7UA', false],
+            ['W1H 22LQ', false],
+            ['W122LQFD', false],
+            ['W1 H2H', false],
+            ['', false],
+            [' ', false]
+        ];
+    }
 
-  public function inputPostcodesParts()
-  {
-    return [
-      ['NE29 0EG', true, 'NE29', '0EG', 'NE', '29', '0', 'EG'],
-      ['B37 7UA', true, 'B37', '7UA', 'B', '37', '7', 'UA'],
-      ['W1H 2LQ', true, 'W1H', '2LQ', 'W', '1H', '2', 'LQ'],
-      ['W2 1JB', true, 'W2', '1JB', 'W', '2', '1', 'JB'],
-      ['NE290EG', false, 'NE290', '90EG', 'N', '1', 'NE', '9'],
-      ['CT139FD', false, 'CT139FD', 'CT139FD', 'CT139FD', 'CT139FD', 'CT139FD', 'CT139FD'],
-    ];
-  }
+    public function inputPostcodesParts()
+    {
+        return [
+            ['NE29 0EG', true, 'NE29', '0EG', 'NE', '29', '0', 'EG'],
+            ['B37 7UA', true, 'B37', '7UA', 'B', '37', '7', 'UA'],
+            ['W1H 2LQ', true, 'W1H', '2LQ', 'W', '1H', '2', 'LQ'],
+            ['W2 1JB', true, 'W2', '1JB', 'W', '2', '1', 'JB'],
+            ['NE290EG', false, 'NE290', '90EG', 'N', '1', 'NE', '9'],
+            ['CT139FD', false, 'CT139FD', 'CT139FD', 'CT139FD', 'CT139FD', 'CT139FD', 'CT139FD'],
+        ];
+    }
 
-  /**
-   * @dataProvider inputPostcodesValidator
-   *
-   * @param string $postcode
-   * @param bool $outcome
-   */
-  public function testPostcodeValidator($postcode, $outcome):void
-  {
-    $this->assertEquals(Postcode::validate($postcode), $outcome);
-  }
+    public function inputFindInString()
+    {
+        return [
+            ['13, Fake Street, Fake Vill, Fake City, Fake Country, NE29 0EG', 'NE290EG'],
+            ['13, Fake Street, Fake Vill, Fake City, Fake Country, NE20EG', 'NE20EG'],
+            ['13, Fake Street, Fake Vill, Fake City, Fake Country, ne290EG', 'NE290EG'],
+            ['13, Fake Street, Fake Vill, Fake City, Fake Country, ne290eg', 'NE290EG'],
+            ['13, Fake Street, Fake Vill, Fake City, Fake Country', ''],
+            ['Fake House Name Fake Street NE29 0EG', 'NE290EG'],
+            ['Fake House Name Fake Street NE29 0EG United Kingdom', 'NE290EG'],
+            ['Fake House Name Fake Street NE29 0EG, United Kingdom', 'NE290EG'],
+            ['Fake House Name Fake Street NE29 0EG United Kingdom CT13 9FD', 'NE290EG'],
+            ['Fake House', ''],
+            ['123', ''],
+            ['Flat1/a 12 Fake Street, Fake Vill', ''],
+            ['Flat1/a 12 Fake Street, Fake Vill NE29 0EG', 'NE290EG'],
+            ['NE29 0EG, CT13 9FD', 'NE290EG'],
+            ['NE290EG CT13 9FD', 'NE290EG'],
+            ['NE29 0EG CT13 9FD', 'NE290EG'],
+            ['NE290EG', 'NE290EG'],
+            ['NE2990EG', ''],
+            ['NE29 90EG', ''],
+            ['NE29 0EG', 'NE290EG'],
+            ['  NE290EG   ', 'NE290EG'],
+            ['', ''],
+            [' ', ''],
+            ['NE 290EG', 'NE290EG'],
+            ['NE29-0EG', 'NE290EG'],
+            ['NE290E G', 'NE290EG'],
+        ];
+    }
 
-  public function testThrowExceptionIfBadPostcodeIsPassed()
-  {
-    $this->expectException(\InvalidArgumentException::class);
-    new Postcode('BANGARANG');
-  }
+    /**
+     * @dataProvider inputPostcodesValidator
+     * @test
+     *
+     * @param string $postcode
+     * @param bool $outcome
+     */
+    public function postcodeValidator($postcode, $outcome): void
+    {
+        $this->assertEquals(Postcode::validate($postcode), $outcome);
+    }
 
-  public function testGetPostcodeBack()
-  {
-    $postcode = 'NE29 0EG';
+    /**
+     * @test
+     */
+    public function throwExceptionIfBadPostcodeIsPassed()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Postcode('BANGARANG');
+    }
 
-    $pcObj = new Postcode($postcode);
+    /**
+     * @test
+     */
+    public function getPostcodeBack()
+    {
+        $postcode = 'NE29 0EG';
 
-    $this->assertEquals(Postcode::format($postcode), $pcObj->get());
-  }
+        $pcObj = new Postcode($postcode);
 
-  /**
-   * @dataProvider inputPostcodesParts
-   *
-   * @param string $postcode
-   * @param bool $outcome
-   * @param string $outward
-   * @param string $inward
-   * @param string $area
-   * @param string $district
-   * @param string $sector
-   * @param string $unit
-   */
-  public function testGetPostcodeParts($postcode, $outcome, $outward, $inward, $area, $district, $sector, $unit)
-  {
-    $pcObj = new Postcode($postcode);
+        $this->assertEquals(Postcode::format($postcode), $pcObj->get());
+    }
 
-    // Outward
-    $this->assertEquals(
-      ($pcObj->getOutward() == $outward),
-      $outcome
-    );
+    /**
+     * @dataProvider inputPostcodesParts
+     * @test
+     *
+     * @param string $postcode
+     * @param bool $outcome
+     * @param string $outward
+     * @param string $inward
+     * @param string $area
+     * @param string $district
+     * @param string $sector
+     * @param string $unit
+     */
+    public function getPostcodeParts($postcode, $outcome, $outward, $inward, $area, $district, $sector, $unit)
+    {
+        $pcObj = new Postcode($postcode);
 
-    // Inward
-    $this->assertEquals(
-      ($pcObj->getInward() == $inward),
-      $outcome
-    );
+        // Outward
+        $this->assertEquals(
+            ($pcObj->getOutward() == $outward),
+            $outcome
+        );
 
-    // Area
-    $this->assertEquals(
-      ($pcObj->getArea() == $area),
-      $outcome
-    );
+        // Inward
+        $this->assertEquals(
+            ($pcObj->getInward() == $inward),
+            $outcome
+        );
 
-    // District
-    $this->assertEquals(
-      ($pcObj->getDistrict() == $district),
-      $outcome
-    );
+        // Area
+        $this->assertEquals(
+            ($pcObj->getArea() == $area),
+            $outcome
+        );
 
-    // sector
-    $this->assertEquals(
-      ($pcObj->getSector() == $sector),
-      $outcome
-    );
+        // District
+        $this->assertEquals(
+            ($pcObj->getDistrict() == $district),
+            $outcome
+        );
 
-    // unit
-    $this->assertEquals(
-      ($pcObj->getUnit() == $unit),
-      $outcome
-    );
-  }
+        // sector
+        $this->assertEquals(
+            ($pcObj->getSector() == $sector),
+            $outcome
+        );
+
+        // unit
+        $this->assertEquals(
+            ($pcObj->getUnit() == $unit),
+            $outcome
+        );
+    }
+
+    /**
+     * @dataProvider inputFindInString
+     * @test
+     *
+     * @param string $term
+     * @param mixed $postcode
+     */
+    public function pullPostCodeFromString(string $term, $postcode = false)
+    {
+        $foundPostcode = Postcode::findInString($term);
+
+        if ($foundPostcode instanceof Postcode) {
+            $foundPostcode = $foundPostcode->get();
+        }
+
+        $this->assertEquals(
+            $postcode,
+            $foundPostcode,
+            "Did not find postcode as expected when pulling from a string"
+        );
+    }
 }
